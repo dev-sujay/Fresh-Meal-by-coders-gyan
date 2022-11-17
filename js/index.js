f2efc7acceb5cb862dc15200565248ce0bb47782
Sujay Paul <sujaypaul531@gmail.com>
2022-11-18 00:52:21 +0530

    made the timer alive with javascript

--- js/index.js
+++ js/index.js
@@ -9,15 +9,15 @@ $(document).ready(function () {
         nextArrow: ".next-btn",
         responsive: [
             {
-                breakpoint:992,
-                settings:{
+                breakpoint: 992,
+                settings: {
                     slidesToShow: 2,
                 }
             },
             {
-                breakpoint:768,
-                settings:{
-                    slidesToShow:1,
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
                 }
             }
 
@@ -31,3 +31,33 @@ const siteContentWrapper = document.querySelector(".site-content-wrapper")
 document.querySelector(".nav-trigger").addEventListener("click", function () {
     siteContentWrapper.classList.toggle("scaled")
 })
+
+
+
+
+
+
+const timerUpdater = () => {
+
+    const endDay = new Date("20 November, 2022 11:59:59").getDate()
+    const endHour = new Date("20 November, 2022 11:59:59").getHours()
+    const endMin = new Date("20 November, 2022 11:59:59").getMinutes()
+    const endSec = new Date("20 November, 2022 11:59:59").getSeconds()
+
+    const d = new Date().getDate()
+    const h = new Date().getHours()
+    const m = new Date().getMinutes()
+    const s = new Date().getSeconds()
+    document.querySelector("#daysLeft").innerHTML = endDay - d
+    document.querySelector("#hoursLeft").innerHTML = endHour - h
+    document.querySelector("#minLeft").innerHTML = endMin - m
+    document.querySelector("#secLeft").innerHTML = endSec - s
+}
+
+
+setInterval(() => {
+    timerUpdater()
+}, 1000);
+
+
+
